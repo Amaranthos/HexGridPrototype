aab6ebdcefd55952c9621329841fea1c57cedea1
C-Syratt <c.syratt@aanet.com.au>
2015-10-19 20:28:54 +1000

    Unit Health Bars
    Know issue: when a unit moves down on the board, the bar's position is
    wrong.

---
+++ Utangard/Assets/UnitHealthBar.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UnitHealthBar : MonoBehaviour {
+
+	private Slider hpBar;
+	private Unit unit;
+	private int tempHp;
+	private bool updatingHP;
+	private bool cooldown;
+	private float counter;
+	private Quaternion initRot;
+
+	void Start () {
+		hpBar = GetComponentInChildren<Slider>();
+		unit = GetComponentInParent<Unit>();
+		hpBar.maxValue = unit.maxHitpoints;
+		tempHp = unit.CurrentHitpoints;
+		hpBar.value = tempHp;
+		UpdateRotation();
+		initRot = transform.rotation;
+	}
+
+	void Update () {
+		UpdateHealthBar();
+	}
+
+	void LateUpdate () {
+		UpdatePosition();
+	}
+
+	void UpdateHealthBar(){
+		if(tempHp != unit.CurrentHitpoints){
+			updatingHP = true;
+		}
+		else{
+			updatingHP = false;
+		}
+
+		if(updatingHP){
+			if(counter >= 0.000001){
+				counter = 0;
+				if(unit.CurrentHitpoints < tempHp){
+					tempHp--;
+				}
+				else{
+					tempHp++;
+				}
+				hpBar.value = tempHp;
+			}
+			else{
+				counter += Time.deltaTime;
+			}
+		}
+	}
+
+	void UpdateRotation(){
+		if(unit.Owner == Logic.Inst.Players[0]){
+			transform.localPosition = new Vector3(Mathf.Abs(transform.localPosition.x), transform.localPosition.y, transform.localPosition.z);
+			transform.rotation = Quaternion.Euler(30,0,0);
+		}
+	}
+
+	void UpdatePosition(){
+		transform.rotation = initRot;
+	}
+}
