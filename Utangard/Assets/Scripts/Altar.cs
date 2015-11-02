04fe7461e22e2da8f98123369c7dcbd37abe73b5
Mechanolith <j.kuskoff@hotmail.com>
2015-11-02 21:39:03 +1000

    Dynamic Altar Flags
    Altar flags now change colour according to their owner.

--- Utangard/Assets/Scripts/Altar.cs
+++ Utangard/Assets/Scripts/Altar.cs
@@ -12,6 +12,20 @@ public class Altar : MonoBehaviour {
 			owner.capturedAltars.Remove(this);
 		owner = player;
 		player.capturedAltars.Add(this);
+
+		if(gameObject.transform.childCount > 0){
+			Transform model = gameObject.transform.FindChild("Alter_nobase");
+			for(int j = 0; j < model.transform.childCount; j++){
+				GameObject child = model.GetChild(j).gameObject;
+				if(child.name == "Plane001" || child.name == "Plane002" || child.name == "Plane003"){
+					if(!child.GetComponent<SkinnedMeshRenderer>()){
+						MeshRenderer meshRend = child.GetComponent<MeshRenderer>();
+						meshRend.material = new Material(meshRend.material);
+						meshRend.material.color = owner.playerColour;
+					}
+				}
+			}
+		}
 	}
 
 	public Player Owner{
