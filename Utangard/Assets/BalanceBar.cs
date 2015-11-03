1612ca3cd333de0d4870dc5f9cdf9dbde003d976
C-Syratt <c.syratt@aanet.com.au>
2015-11-03 17:24:04 +1000

    Balance Bar
    Bar changes to match Altars captured. Colours change to respective team
    colours.

---
+++ Utangard/Assets/BalanceBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BalanceBar : MonoBehaviour {
+
+	Slider bar;
+	public Image background;
+	public Image fill;
+
+	// Use this for initialization
+	void Start () {
+		bar = GetComponentInChildren<Slider>();
+		bar.value = Logic.Inst.Players[0].capturedAltars.Count;
+		background.color = Logic.Inst.Players[1].playerColour;
+		fill.color = Logic.Inst.Players[0].playerColour;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bar.value = Logic.Inst.Players[0].capturedAltars.Count;
+	}
+}
