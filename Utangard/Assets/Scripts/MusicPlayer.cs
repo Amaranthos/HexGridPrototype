c90f41e05e76f9a83201e79e1dc3b3196a071971
Amaranthos <joshua.hodkinson.42@gmail.com>
2015-11-14 13:14:48 +1000

    Music transitions in progress

--- Utangard/Assets/Scripts/MusicPlayer.cs
+++ Utangard/Assets/Scripts/MusicPlayer.cs
@@ -1,12 +1,60 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
 
-	public AudioSource bloodOath;
-	public AudioSource placingTheme;
-	public AudioSource battleTheme;
-	public AudioSource winningTheme;
+	public AudioMixerSnapshot title;
+	public AudioMixerSnapshot placing;
+	public AudioMixerSnapshot battle;
+	public AudioMixerSnapshot winning;
 
-	
+	public AudioMixerSnapshot mute;
+
+	private float bpm = 128;
+	private float timeIn = 0;
+	private float timeOut = 0;
+	private float quaterNote = 0;
+
+	private AudioMixerSnapshot current;
+
+	void Awake() {
+		quaterNote = 60 / bpm;
+		timeIn = quaterNote;
+		timeOut = quaterNote * 32;
+	}
+
+	public void ChangeBase(MusicBaseState theme){
+		switch(theme){
+			case MusicBaseState.Title:
+				current = title;
+				break;
+
+			case MusicBaseState.Placing:
+				current = placing;
+				break;
+
+			case MusicBaseState.Battle:
+				current = battle;
+				break;
+
+			case MusicBaseState.NearWin:
+				current = winning;
+				break;
+		}
+
+		current.TransitionTo(timeIn);
+	}
+
+	public void Mute(){
+		mute.TransitionTo(0f);
+	}
+}
+
+[System.Serializable]
+public enum MusicBaseState {
+	Title,
+	Placing,
+	Battle,
+	NearWin
 }
