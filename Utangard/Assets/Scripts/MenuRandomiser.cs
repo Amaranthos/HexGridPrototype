c07728d2835e84136ec30436b6df51e7d0c82f7f
C-Syratt <c.syratt@aanet.com.au>
2015-11-26 22:22:19 +1000

    Main Menu Visuals
    For the time being, only choices between Thor and Eir.

---
+++ Utangard/Assets/Scripts/MenuRandomiser.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuRandomiser : MonoBehaviour {
+
+	public Material[] mats; // 1 - Eir, 2 - Heimy, 3 - Skadi, 4 - Thor
+	public GameObject[] heroModels;
+	public GameObject banner;
+
+	// Use this for initialization
+	void Start () {
+		int h = Random.Range(0,heroModels.Length);
+		for(int i = 0; i < heroModels.Length; i++){
+			if(i == h){
+				heroModels[i].SetActive(true);
+			}
+			else{
+				heroModels[i].SetActive(false);
+			}
+		}
+
+		for(int i = 0; i < mats.Length; i++){
+			if(i == h){
+				banner.GetComponent<MeshRenderer>().material = mats[i];
+			}
+		}
+	}
+}
