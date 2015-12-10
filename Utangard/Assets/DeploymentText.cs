86d93e68d98b60051228b7da354ff9fc539f3ada
C-Syratt <c.syratt@aanet.com.au>
2015-12-10 22:20:32 +1000

    Formation Button Icons
    Plus the deploy text now displays the correct player's turn.

---
+++ Utangard/Assets/DeploymentText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DeploymentText : MonoBehaviour {
+
+    public Text playerTurn;
+
+    void Update() {
+        
+        if (Logic.Inst.CurrentPlayerNum == 0)
+        {
+            playerTurn.text = "Player 1's Turn";
+        }
+        else 
+        {
+            playerTurn.text = "Player 2's Turn";
+        }
+    }
+
+}
